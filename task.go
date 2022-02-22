1cbf5f07a17774863820a15db9a4f17b0624ec2f
Rostyslav Popadyn <rostykpopadynrostykpopadyn451@gmail.com>
2022-02-22 14:43:42 +0200

    added averageNumber declaration

--- task.go
+++ task.go
@@ -4,18 +4,29 @@ import (
 	"regexp"
 )
 
+var validFmt = regexp.MustCompile(`(\d+-[[:ascii:]]+-)+$`)
+
 // testValidity - validates input string.
 // Expected a sequence of numbers followed by dash followed by text, eg: `23-ab-48-caba-56-haha`
 // Time complexity: O(N)
 // Estimated time: 20m
 // Used time: 28m
-
-var validFmt = regexp.MustCompile(`(\d+-[[:ascii:]]+-)+$`)
-
 func testValidity(s string) bool {
 	if len(s) == 0 {
 		return false
 	}
 
 	return validFmt.MatchString(s + "-")
+}
+
+// averageNumber - calculates average number from all the numbers
+// Time complexity: O(N)
+// Estimated time: 10m
+// Used time: ?m
+func averageNumber(s string) float64 {
+	if len(s) == 0 {
+		return 0
+	}
+
+	return 0
 }
