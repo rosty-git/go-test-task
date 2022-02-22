7653e26ac551e43cedcabac930bc6f4d3eba4b69
Rostyslav Popadyn <rostykpopadynrostykpopadyn451@gmail.com>
2022-02-22 14:40:26 +0200

    added testValidity implementation

--- task.go
+++ task.go
@@ -1,10 +1,21 @@
 package main
 
+import (
+	"regexp"
+)
+
 // testValidity - validates input string.
 // Expected a sequence of numbers followed by dash followed by text, eg: `23-ab-48-caba-56-haha`
 // Time complexity: O(N)
 // Estimated time: 20m
-// Used time: ?m
+// Used time: 28m
+
+var validFmt = regexp.MustCompile(`(\d+-[[:ascii:]]+-)+$`)
+
 func testValidity(s string) bool {
-    return false
+	if len(s) == 0 {
+		return false
+	}
+
+	return validFmt.MatchString(s + "-")
 }
