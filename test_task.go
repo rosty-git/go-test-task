48940781360d47c5a4c8cab7f55d8647fc1ee180
Rostyslav Popadyn <rostykpopadynrostykpopadyn451@gmail.com>
2022-02-22 15:21:42 +0200

    added generate function implementation

--- test_task.go
+++ test_task.go
@@ -1,9 +1,54 @@
 package main
 
+import (
+	"math/rand"
+	"strconv"
+	"strings"
+	"time"
+)
+
 // generate - generates random correct strings if the parameter is `true` and random incorrect strings otherwise
 // Time complexity: O(N)
 // Estimated time: 20m
-// Used time: ?m
+// Used time: 30m
 func generate(correct bool) string {
-    return ""
+	rand.Seed(time.Now().UnixNano())
+
+	if !correct && rand.Float32() < 0.1 {
+		return ""
+	}
+
+	tokenPairs := rand.Intn(10) + 1
+	tokens := make([]string, 0, tokenPairs*2)
+
+	for i := 0; i < tokenPairs; i++ {
+		if correct {
+			tokens = append(tokens, genNumber())
+		} else {
+			tokens = append(tokens, genWord())
+		}
+		tokens = append(tokens, genWord())
+	}
+
+	return strings.Join(tokens, "-")
+}
+
+// genNumber - generates random number
+func genNumber() string {
+	return strconv.Itoa(rand.Intn(1000))
+}
+
+const asciiLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+// genWord - generates random string
+func genWord() string {
+	wordLen := rand.Intn(15) + 1
+	var word strings.Builder
+	word.Grow(wordLen)
+
+	for i := 0; i < wordLen; i++ {
+		word.WriteByte(asciiLetters[rand.Intn(len(asciiLetters))])
+	}
+
+	return word.String()
 }
