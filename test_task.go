6fa837ad03856e79af37113a53936fd44095d0a5
Rostyslav Popadyn <rostykpopadynrostykpopadyn451@gmail.com>
2022-02-22 15:18:55 +0200

    added generate function declaration

---
+++ test_task.go
@@ -0,0 +1,9 @@
+package main
+
+// generate - generates random correct strings if the parameter is `true` and random incorrect strings otherwise
+// Time complexity: O(N)
+// Estimated time: 20m
+// Used time: ?m
+func generate(correct bool) string {
+    return ""
+}
