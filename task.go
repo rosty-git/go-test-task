3e0173854a254198eb67e12940bab5af00c46844
Rostyslav Popadyn <rostykpopadynrostykpopadyn451@gmail.com>
2022-02-22 13:52:48 +0200

    added testValidity declaration

---
+++ task.go
@@ -0,0 +1,10 @@
+package main
+
+// testValidity - validates input string.
+// Expected a sequence of numbers followed by dash followed by text, eg: `23-ab-48-caba-56-haha`
+// Time complexity: O(N)
+// Estimated time: 20m
+// Used time: ?m
+func testValidity(s string) bool {
+    return false
+}
